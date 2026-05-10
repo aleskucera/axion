50682ff393bf5a2a367a3e8210b646f0e3b7d9fd
marosmester <mestermaros@gmail.com>
2026-05-10 19:20:38 +0200

    timings: plot new measurement

--- src/axion/neural_solver/comparisons/axion_timing_plot.py
+++ src/axion/neural_solver/comparisons/axion_timing_plot.py
@@ -1,39 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#                                    mean       std
-control = np.array([0.013548, 0.000475])
-initial_guess = np.array([0.056488, 0.000753])
-newton_linearization = np.array([0.062619, 0.000750])
-newton_linear_solve = np.array([0.492124, 0.026524])
-#newton_linesearch = np.array([0.592459, 0.020963])
-all = np.stack(
-    [
-        control,
-        initial_guess,
-        newton_linearization,
-        newton_linear_solve,
-    ],
-    axis=0,
-)
-
-means = all[:, 0]
-stds = all[:, 1]
+tick_fontsize = 13
+legend_fontsize = 13
 
 labels = [
-    "control calculation",
-    "newton: initial_guess",
-    "newton: system linearization",
-    "newton: linear solve",
+    "Collision\n detection",
+    "Contact\n preprocessing",
+    "Newton's method\ninitial guess",
+    "Newton system\nsolving",
+    "Best iterate\nbacktracking",
+    "Output\ncopying",
 ]
+means = np.array([0.350, 1.272, 0.015, 8.734, 0.026, 0.020])
+p95s = np.array([0.369, 1.598, 0.014, 13.877, 0.023, 0.014])
+
 x = np.arange(len(labels))
+width = 0.4
 
 fig, ax = plt.subplots()
 ax.set_axisbelow(True)
-ax.bar(x, means, yerr=stds, capsize=4, ecolor="black", color="steelblue", edgecolor="black")
-ax.set_xticks(x, labels, rotation=45, ha="right")
-ax.set_ylabel("Time [ms]")
+ax.bar(x - width / 2, means, width, label="mean", color="steelblue", edgecolor="black")
+ax.bar(x + width / 2, p95s, width, label="p95", color="indianred", edgecolor="black")
+ax.set_xticks(x, labels, rotation=0, ha="center", fontsize = tick_fontsize)
+ax.set_ylabel("Time [ms]", fontsize=tick_fontsize)
+ax.tick_params(axis="y", labelsize=tick_fontsize)
 ax.grid(axis="y", linestyle="--", alpha=0.6, color="gray")
-ax.set_title("Axion Simulator Timing experiments")
+ax.set_title("Axion simulator timing experiments (end to end)")
+ax.legend(fontsize=legend_fontsize)
 fig.tight_layout()
 plt.show()
